dac876ebd9a45bb55d50e2b2fe57daaa505c8d2d
ElderSousa <elder2faces@gmail.com>
2025-05-14 12:19:53 -0300

    ValidationUser Concluido

---
+++ MS-AuthenticationAutorization.API/Program.cs
@@ -0,0 +1,62 @@
+using System.Reflection;
+using Asp.Versioning;
+using CroosCutting.MS_AuthenticationAutorization.IoC;
+using CroosCutting.MS_AuthenticationAutorization.Middleware;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+//Versionamento
+builder.Services.AddApiVersioning(v =>
+{
+    v.DefaultApiVersion = new ApiVersion(1.0);
+    v.AssumeDefaultVersionWhenUnspecified = true;
+    v.ReportApiVersions = true;
+    v.ApiVersionReader = ApiVersionReader.Combine(
+            new UrlSegmentApiVersionReader(),
+            new QueryStringApiVersionReader()
+    );
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.InjectDataBase(builder.Configuration);
+builder.Services.InjectDependency();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "MS-AuthenticatonAutorization",
+        Description = "Autenticação e autorização de usúários"
+    });
+    var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    opts.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
